9eee232a2b15827683c1ddd1fe09ac56bcb3eacf
glody thakizimana <glody.thakizimana@outlook.fr>
2021-09-24 10:35:33 +0200

    first commit

---
+++ app/Models/Character.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Character extends Model
+{
+    use HasFactory;
+    
+    public function charectes()
+    {
+        return $this->hasMany(Character::class);
+    }
+}
---
+++ database/migrations/2021_09_24_075946_create_characters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('year_creation');
+            $table->string('designer');
+            $table->string('affiliate name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
